e696d0c9ab28bbf91309f8e5ee7216e5a591416b
dicosta <dicosta@gmail.com>
2013-01-02 01:47:23 -0300

    adds solution startup, model, mongo repositories and mongo provider

---
+++ RssReader.Model.Mongo/Repositories/ReadOnlyRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using RssReader.Model.Contracts;
+
+namespace RssReader.Model.Mongo.Repositories
+{
+    public class ReadOnlyRepository<T> : IReadOnlyRepository<T> where T : class
+    {
+        protected MongoCollection<T> _collection { get; set; }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return _collection.AsQueryable<T>();
+        }
+    }
+}
---
+++ RssReader.Model.Mongo/Repositories/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RssReader.Model.Contracts;
+using MongoDB.Driver.Linq;
+
+namespace RssReader.Model.Mongo.Repositories
+{
+    public class Repository<T> : ReadOnlyRepository<T>, IRepository<T> where T : class
+    {
+        public virtual void Add(T newItem)
+        {
+            base._collection.Insert(newItem);
+        }
+
+        public virtual void Add(IEnumerable<T> newItems)
+        {
+            base._collection.Insert(newItems);
+        }
+
+        public virtual void Update(T item)
+        {
+            base._collection.Save(item);
+        }
+
+        public virtual void Update(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                base._collection.Save(item);
+            }
+        }
+
+        public virtual void DeleteAll()
+        {
+            base._collection.RemoveAll();
+        }
+
+        public virtual int Count()
+        {
+            return (int)base._collection.Count();
+        }
+
+        public virtual bool Exists(System.Linq.Expressions.Expression<Func<T, bool>> criteria)
+        {
+            return base._collection.AsQueryable<T>().Any(criteria);
+        }
+    }
+}
