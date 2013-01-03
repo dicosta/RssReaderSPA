c1934fb2f2b325b3da6869841981dc290dfb2ec2
Diego Costa <diego.costa@hobsons.com>
2013-01-03 01:20:12 -0300

    adds uow infrastructure for mongo and mongocollection provider for mongo repositories

---
+++ RssReader.Model.Mongo/Configuration/MongoModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+using RssReader.Model.Contracts;
+using RssReader.Model.Mongo.Infrastructure;
+using RssReader.Model.Mongo.Infrastructure.Contracts;
+
+namespace RssReader.Model.Mongo.Configuration
+{
+    public class MongoModule : NinjectModule
+    {
+        public override void Load()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ReaderRssMongoDB"].ConnectionString;
+
+            IMongoCollectionProvider collectionProvider = new MongoCollectionProvider(connectionString);
+
+            Bind<IMongoCollectionProvider>().ToConstant(collectionProvider)
+                .InSingletonScope();
+
+            //In Web Project.
+            /*
+            Bind<IUnitOfWork>().To<MongoUnitOfWork>()
+                .InRequestScope();
+            */
+        }
+    }
+}
---
+++ RssReader.Model/Contracts/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssReader.Model.Contracts
+{
+    public interface IUnitOfWork
+    {
+        void Commit();
+        void Rollback();
+    }
+}
